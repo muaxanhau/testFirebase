1ddfc081ee847359af4556dfc0c30476c61387a7
muaxanhau <thinhhoang17485@gmail.com>
2024-04-02 13:18:05 +0700

    optimize tabLayout

--- src/repositories/items/getItemsByCategoryId.repo.ts
+++ src/repositories/items/getItemsByCategoryId.repo.ts
@@ -2,6 +2,7 @@ import {KeyService, service, useApiInfiniteQuery} from 'repositories/services';
 import {ItemIdModel, PaginationResponseBaseModel} from 'models';
 import {utils} from 'utils';
 import {devToolConfig} from 'config';
+import {useQueryClient} from '@tanstack/react-query';
 
 export type GetItemsByCategoryIdProps = {id: string};
 export type GetItemsByCategoryIdOutput = PaginationResponseBaseModel<{
@@ -10,33 +11,40 @@ export type GetItemsByCategoryIdOutput = PaginationResponseBaseModel<{
 export const useGetItemsByCategoryIdRepo = ({
   id,
 }: GetItemsByCategoryIdProps) => {
-  const {data, hasNextPage, ...rest} =
-    useApiInfiniteQuery<GetItemsByCategoryIdOutput>({
-      queryKey: [KeyService.GET_ITEMS_BY_CATEGORY_ID, id],
-      queryFn: async ({pageParam}) => {
-        await utils.sleep(devToolConfig.delayFetching);
+  const queryClient = useQueryClient();
 
-        const response = await service.get<GetItemsByCategoryIdOutput>(
-          'items',
-          {
-            params: {
-              categoryId: id,
-              page: pageParam,
-            },
-          },
-        );
-        return response.data;
-      },
-      initialPageParam: 0,
-      getNextPageParam: lastPage =>
-        lastPage.totalPage === lastPage.nextPage
-          ? undefined
-          : lastPage.nextPage,
+  const {
+    data,
+    refetch: refetchData,
+    ...rest
+  } = useApiInfiniteQuery<GetItemsByCategoryIdOutput>({
+    queryKey: [KeyService.GET_ITEMS_BY_CATEGORY_ID, id],
+    queryFn: async ({pageParam}) => {
+      await utils.sleep(devToolConfig.delayFetching);
+
+      const response = await service.get<GetItemsByCategoryIdOutput>('items', {
+        params: {
+          categoryId: id,
+          page: pageParam,
+        },
+      });
+      return response.data;
+    },
+    initialPageParam: 0,
+    getNextPageParam: lastPage =>
+      lastPage.totalPage === lastPage.nextPage ? undefined : lastPage.nextPage,
+  });
+
+  const refetch = async () => {
+    await queryClient.resetQueries({
+      queryKey: [KeyService.GET_ITEMS_BY_CATEGORY_ID, id],
     });
+    refetchData();
+  };
 
   const items = data?.pages.reduce<ItemIdModel[]>((prev, curr) => {
     return [...prev, ...curr.items];
   }, []);
 
-  return {items, ...rest};
+  return {items, refetch, ...rest};
 };
